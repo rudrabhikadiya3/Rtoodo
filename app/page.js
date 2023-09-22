1fbdf8a0ccbf5f327f799a3f6824ae67970542b1
Rudra Bhikadiya <irudradev@gmail.com>
2023-09-22 15:26:56 +0530

    styling

--- app/page.js
+++ app/page.js
@@ -1,94 +1,48 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='background'>
+      <div className='logo'>Rtoodo</div>
+      <div className='container'>
+        <div className='row align-items-center justify-content-center flex-column' style={{ height: '100vh' }}>
+          <div className='card col-md-6 col-12 mb-4'>
+            <div className='card-body'>
+              <div className='taskpad'>
+                <button>
+                  <i className='bi bi-plus-lg' />
+                </button>
+                <input type='text' name='task' placeholder="Let's complete incompleted..." />
+              </div>
+            </div>
+          </div>
+          <div className='card col-md-6 col-12 p-0'>
+            <div className='card-body p-0'>
+              <div className='tasks border-bottom border-dark px-4'>
+                <label className='chekBoxContainer'>
+                  <input type='checkbox' />
+                  <div className='checkmark'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'>
+                      <title>Checkmark</title>
+                      <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth={32} d='M416 128L192 384l-96-96' />
+                    </svg>
+                  </div>
+                </label>
+                <p className='m-0'>Read for 8 Minutes</p>
+              </div>
+              <div className='tasks border-dark px-4 completed'>
+                <label className='chekBoxContainer'>
+                  <input type='checkbox' defaultChecked='checked' />
+                  <div className='checkmark'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'>
+                      <title>Checkmark</title>
+                      <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth={32} d='M416 128L192 384l-96-96' />
+                    </svg>
+                  </div>
+                </label>
+                <p className='m-0'>Read for 8 Minutes</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
